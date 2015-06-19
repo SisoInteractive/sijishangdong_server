6eb7d5f320de6e2963afa60979c23c63dd8a0dae
Qianglong Mo <sammok2003@hotmail.com>
2015-06-19 12:59:33 +0800

    Rewrite server with socket.io .

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
-    multer = require('multer'),
     MongoClient = require('mongodb');
 var server = require("http").Server(app);
 var socket = require("socket.io").listen(server);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));  //  parser url
-app.use(multer());
-
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+server.listen(88, '120.26.48.94');
+console.log('Server running at 120.26.48.94:88');
 
 //  connect to mongo
 MongoClient.connect('mongodb://localhost:27017/sijishangdong', function (err, db) {
@@ -40,68 +18,88 @@ MongoClient.connect('mongodb://localhost:27017/sijishangdong', function (err, db
         ticketIdList = docs[0]['idlist'];
     });
 
-    socket.on('choujiang', onChoujiangHandler);
-    socket.on('userInfo', onGetUserInfoHandler);
+    /** socket on */
+        // Run the server
+    init();
 
-    function onChoujiangHandler (socket) {
-        var result = '';
+    // Initialization
+    function init() {
+        setEventHandlers();
+    }
 
-        console.log('someone join to this game');
+    // Event handlers
+    function setEventHandlers() {
+        // Socket.IO
+        socket.on("connection", onSocketConnection);
+    }
 
-        db.collection('visitedNumber').update({"visitedNumber": {$exists: 1}}, {$inc: {"visitedNumber": 1}});
 
-        var visitedNumber = null;
+    // New socket connection
+    function onSocketConnection(socket) {
+        console.log('Someone comes here!!!!');
 
-        db.collection("visitedNumber").find({"visitedNumber": {$exists: 1}}).toArray(function (err, docs) {
-            visitedNumber = docs[0]['visitedNumber'];
+        socket.on('choujiang', onChoujiangHandler);
+        socket.on('userinfo', onGetUserInfoHandler);
 
-            if (visitedNumber > 2400) {
-                //  return false
-                console.log(visitedNumber  + ':' + 10086);
-                result = 10086;
-            } else {
-                var isGetGift = false;
+        function onChoujiangHandler (socket) {
+            var that = this;
+            var result = '';
 
-                for (var i = 0; i < ticketIdList.length; i++) {
-                    //console.log(visitedNumber, ":::::" , ticketIdList[i]);
-                    if (visitedNumber == ticketIdList[i]) {
-                        isGetGift = true;
-                        break;
+            console.log('someone join to this game');
+
+            db.collection('visitedNumber').update({"visitedNumber": {$exists: 1}}, {$inc: {"visitedNumber": 1}});
+
+            var visitedNumber = null;
+
+            db.collection("visitedNumber").find({"visitedNumber": {$exists: 1}}).toArray(function (err, docs) {
+                visitedNumber = docs[0]['visitedNumber'];
+
+                if (visitedNumber > 2400) {
+                    //  return false
+                    console.log(visitedNumber  + ':' + 10086);
+                    result = 10086;
+                } else {
+                    var isGetGift = false;
+
+                    for (var i = 0; i < ticketIdList.length; i++) {
+                        //console.log(visitedNumber, ":::::" , ticketIdList[i]);
+                        if (visitedNumber == ticketIdList[i]) {
+                            isGetGift = true;
+                            break;
+                        }
+                    }
+
+                    if (isGetGift) {
+                        console.log(visitedNumber  + ':' + 200);
+                        result = 200;
+                    } else {
+                        console.log(visitedNumber  + ':' + 10010);
+                        result = 10010;
                     }
                 }
 
-                if (isGetGift) {
-                    console.log(visitedNumber  + ':' + 200);
-                    result = 200;
-                } else {
-                    console.log(visitedNumber  + ':' + 10010);
-                    result = 10010;
-                }
-            }
-
-            //  return result
-            socket.emit('gameResult', {"result": result});
-        });
-    }
-
-    function onGetUserInfoHandler (data) {
-        console.log("Some one post they info...:");
-        var data = data;
-
-        if (data.username || data.phone) {
-            console.log('err');
-        } else {
-            console.log(data.username, data.phone);
-            //  insert data to database
-            db.collection('userInfo').insert(data, function (err, data) {
-
+                //  return result
+                that.emit('gameresult', {"result": result});
             });
         }
 
-        socket.emit('storeInfoResult', {"result": true});
-    }
+        function onGetUserInfoHandler (data) {
+            console.log("Some one post they info...:", data);
+            var userInfo = data;
 
-    app.listen(88, "120.26.48.94");
-    console.log('Server running at 120.26.48.94:88');
+            //  check is two parameters empty
+            if (!userInfo.username || !userInfo.phone) {
+                console.log('err');
+            } else {
+                console.log(userInfo.username, userInfo.phone);
+                //  insert data to database
+                db.collection('userInfo').insert(userInfo, function (err, data) {
+
+                });
+            }
+
+            this.emit('storeduser', {"result": true});
+        }
+    }
 });
 
